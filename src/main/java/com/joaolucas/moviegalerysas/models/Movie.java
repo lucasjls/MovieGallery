d73ce2c5ded924512999bba398edfafa81bdb8a4
joaolucas <lucasjls_gomes@hotmail.com>
2018-08-26 15:13:46 -0300

    Adicionando model Movie

--- src/main/java/com/joaolucas/moviegalerysas/models/Movie.java
+++ src/main/java/com/joaolucas/moviegalerysas/models/Movie.java
@@ -1,4 +1,30 @@
-package com.joaolucas.moviegalerysas.repositories.Models;
+package com.joaolucas.moviegalerysas.models;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+import java.util.List;
+
+@JsonAutoDetect
 public class Movie {
+
+    private String title;
+    private String pathPoster;
+    private String originalLanguage;
+    private List<Integer> generesIds;
+    private String overview;
+    private Boolean isAdult;
+
+    public Movie(){
+
+    }
+
+    public Movie(String title, String pathPoster, String originalLanguage, List<Integer> generesIds, String overview, Boolean isAdult) {
+        this.title = title;
+        this.pathPoster = pathPoster;
+        this.originalLanguage = originalLanguage;
+        this.generesIds = generesIds;
+        this.overview = overview;
+        this.isAdult = isAdult;
+    }
+
 }
