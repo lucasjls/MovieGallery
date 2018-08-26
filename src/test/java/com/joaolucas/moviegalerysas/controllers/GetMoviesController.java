d0cdb5b08da6da02d34a03523f4ccc073086d184
joaolucas <lucasjls_gomes@hotmail.com>
2018-08-26 15:12:43 -0300

    Adicionando HomeController

--- src/test/java/com/joaolucas/moviegalerysas/controllers/GetMoviesController.java
+++ src/test/java/com/joaolucas/moviegalerysas/controllers/GetMoviesController.java
@@ -1,4 +1,14 @@
 package com.joaolucas.moviegalerysas.controllers;
 
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class GetMoviesController {
+
+    @Test
+    public void GetPopularMoviesSuccessful(){
+
+    }
+
 }
