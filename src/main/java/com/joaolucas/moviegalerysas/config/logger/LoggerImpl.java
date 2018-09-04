e467da574014bd199c67de7c3ae8884d17097487
joaolucas <lucasjls_gomes@hotmail.com>
2018-09-04 02:08:01 -0300

    Alterando classe logger

--- src/main/java/com/joaolucas/moviegalerysas/config/logger/LoggerImpl.java
+++ src/main/java/com/joaolucas/moviegalerysas/config/logger/LoggerImpl.java
@@ -1,29 +1,26 @@
 package com.joaolucas.moviegalerysas.config.logger;
 
-import com.joaolucas.moviegalerysas.config.logger.interfaces.Logger;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-public class LoggerImpl implements Logger {
+public class LoggerImpl {
 
-    private final org.slf4j.Logger logger;
+    private final Logger logger;
 
     public LoggerImpl(){
-        this.logger = LoggerFactory.getLogger(this.getClass());
+        this.logger = (Logger) LoggerFactory.getLogger(this.getClass());
     }
 
-    @Override
     public void info(String message) {
         logger.info(message);
     }
 
-    @Override
     public void debug(String message) {
         logger.debug(message);
     }
 
-    @Override
     public void error(String message) {
         logger.error(message);
     }
